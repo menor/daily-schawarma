ff8714e2535f1ad797da3c72651f9c3903262ed6
menor <josemenor@gmail.com>
2015-03-17 15:27:06 +0100

    Add .gitignore, gulpfile.js and package.json.

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+/*
+  Configuration is based on
+  https://github.com/greypants/gulp-starter
+ 
+  To add a new task, simply add a new task file that directory.
+  gulp/tasks/default.js specifies the default set of tasks to run
+  when you run `gulp`.
+*/
+
+var requireDir = require('require-dir');
+
+// Require all tasks in gulp tasks including subfolders.
+requireDir('./gulp/tasks', { recurse: true });
