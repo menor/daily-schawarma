5faf9cc41984632a5dcdb1208954e1060be88a29
menor <josemenor@gmail.com>
2015-03-18 16:32:37 +0100

    Add bourbon and neat libraries as npm modules. Add them to the compile route for sass.

--- gulpfile.js/tasks/sass.js
+++ gulpfile.js/tasks/sass.js
@@ -2,11 +2,14 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var config = require('../config').sass;
+var neat = require('node-neat').includePaths;
 
 gulp.task('sass', function() {
   return gulp.src(config.src)
     .pipe(sourcemaps.init())
-    .pipe(sass(config.settings))
+    .pipe(sass({
+            includePaths: ['styles  '].concat(neat)
+        }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.dest));
 });
