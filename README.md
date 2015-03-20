455195014a914cff7cb71eec499669dbbb67739a
menor <josemenor@gmail.com>
2015-03-20 15:10:45 +0100

    Add scss linter.

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # daily-schawarma
 An asset pipeline to help with front end development at Resmio. Based on the awesome [gulp-starter](https://github.com/greypants/gulp-starter) but with some sauce of our own.
 
+## Requirements ##
+For the scss linter you need ruby installed, and the install the scss-lint gem
+with `gem install scss-lint`
 
 ## To run it ##
   1- Clone the repo
--- gulpfile.js/tasks/sass.js
+++ gulpfile.js/tasks/sass.js
@@ -2,15 +2,19 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var handleErrors = require('../lib/handleErrors');
 var config = require('../config').sass;
 var neat = require('node-neat').includePaths;
+var scsslint = require('gulp-scss-lint');
 
 gulp.task('sass', function() {
   return gulp.src(config.src)
     .pipe(sourcemaps.init())
+    .pipe(scsslint())
     .pipe(sass({
             includePaths: ['styles  '].concat(neat)
         }))
+     .on('error', handleErrors)
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.dest))
     .pipe(browserSync.reload({stream:true}));
