51f3f9214f8070f63d823b256308d28a1078b27a
menor <josemenor@gmail.com>
2015-03-20 15:52:42 +0100

    Make scss lint only track changed files.

--- gulpfile.js/tasks/sass.js
+++ gulpfile.js/tasks/sass.js
@@ -6,10 +6,12 @@ var handleErrors = require('../lib/handleErrors');
 var config = require('../config').sass;
 var neat = require('node-neat').includePaths;
 var scsslint = require('gulp-scss-lint');
+var cache = require('gulp-cached');
 
 gulp.task('sass', function() {
   return gulp.src(config.src)
     .pipe(sourcemaps.init())
+    .pipe(cache(scsslint)) // We cache the linter so only run it on changes
     .pipe(scsslint())
     .pipe(sass({
             includePaths: ['styles  '].concat(neat)
