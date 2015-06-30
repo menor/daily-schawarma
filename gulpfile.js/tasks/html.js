56529c1e79ee7cbb851075ee16853f54275f69ac
menor <josemenor@gmail.com>
2015-06-30 08:54:25 +0200

    Remove previous version stuff

--- gulpfile.js/tasks/html.js
+++
@@ -1,9 +0,0 @@
-var browserSync = require('browser-sync');
-var config = require('../config').html;
-var gulp = require('gulp');
-
-gulp.task('html', function() {
-  return gulp.src(config.src)
-    .pipe(gulp.dest(config.dest))
-    .pipe(browserSync.reload({stream:true}));
-});
--- gulpfile.js/tasks/watch.js
+++
@@ -1,8 +0,0 @@
-var gulp   = require('gulp');
-var config = require('../config');
-var watch  = require('gulp-watch');
-
-gulp.task('watch', ['default', 'browserSync'], function(callback) {
-  watch(config.sass.watch, function() { gulp.start('sass'); });
-  watch(config.html.watch, function() { gulp.start('html'); });
-});
