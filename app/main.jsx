f2485d9b26bd0a36dd9b8c4216b3e800b253c441
menor <josemenor@gmail.com>
2015-07-03 10:28:22 +0200

    Update readme

--- app/main.jsx
+++ app/main.jsx
@@ -3,11 +3,11 @@ import './stylesheets/main.css';
 import React from 'react';
 import App from './components/App';
 
-main();
-
 function main() {
-  var app = document.createElement('div');
+  const app = document.createElement('div');
   document.body.appendChild(app);
 
   React.render(<App />, app);
 }
+
+main();
